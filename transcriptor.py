e2a8105028f0d20ba57f7e9bd0103ded3b59266d
Clemens Draschba <clemens.draschba@krause-automation.com>
2026-04-30 08:41:14 +0200

    remove old files and rename main program

--- transcriptor.py
+++ transcriptor.py
@@ -29,14 +29,14 @@ Und zusätzlich, sofern die Daten angegeben sind:
 *   Namen der Trauernden als Array von Strings,
 *   Angaben zum Begräbnis, Beisetzung oder Trauerfeier im Feld "Beisetzung" als String,
 *   Sofern Bildinformatinen in Form von Abbildungen enthalten sind beschreibe diese in einem Feld “Grafik” als String,
-*   Beschreibe den Hintergrund im Feld "Hintergrund" als String, jedoch nur wenn dieser Besonderheiten aufweist. Ignoriere einfache weiße Hintergründe.
+*   Beschreibe den Hintergrund im Feld "Hintergrund" als String, jedoch nur wenn dieser Besonderheiten aufweist. Ignoriere einfache einfarbige Hintergründe.
 *   Weitere Angaben als “Bemerkungen” als einzelnes Textfeld.
 
 Lasse Felder zu denen keine Angaben gemacht wurden leer, gebe aber die Feldnamen mit aus.
 """
 
 # Unterstützte Bildformate
-VALID_EXTENSIONS = (".png", ".jpg", ".jpeg", ".bmp", ".gif", ".tiff")
+VALID_EXTENSIONS = (".png", ".jpg", ".jpeg")
 
 
 # ==============================
